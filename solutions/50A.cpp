0d3c01afaefb3ecfee9715072735054f0cb1058f
No_Name <noname7n11@gmail.com>
2026-04-25 11:11:13 +0530

    Question 50A

---
+++ solutions/50A.cpp
@@ -0,0 +1,13 @@
+// Domino piling
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int M, N, max_d;
+    cin >> M >> N;
+    int arrb = M*N;
+    int arrd = 2*1;
+    max_d = arrb/arrd;
+    cout << max_d;
+}
