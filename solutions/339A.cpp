354cd8173cd7de6df4967cce310c677c0ab3a0f4
No_Name <noname7n11@gmail.com>
2026-05-15 22:54:52 +0530

    solved 236A

--- solutions/339A.cpp
+++ solutions/339A.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
+    int j=0;
     string s;
     cin >> s;
+    vector<int> x = {};
 
-    vector<int> x;
-    for (int i = 0; i < (int)s.size(); i += 2) {
-        x.push_back(s[i] - '0');
+    for(int i=0; i < s.length(); i+=2){
+        x.push_back(s[i]);
+        j++;
     }
 
-    sort(x.begin(), x.end());
+    for(int i=0; i < x.size(); i++){
+        if(x[i] > x[i+1]){
+            int c = x[i];
+            x[i] == x[i+1];
+            x[i+1] == c;
+        }
+    }
 
-    for (int i = 0; i < (int)x.size(); ++i) {
-        if (i) cout << '+';
-        cout << x[i];
+    for(int i=0; i < x.size(); i++){
+        cout << x[i] << "+";
     }
     return 0;
 }
