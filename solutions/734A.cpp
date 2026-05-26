d6829375794ffbfeb43b730ce6399d185d3bfee2
No_Name <noname7n11@gmail.com>
2026-05-26 20:45:16 +0530

    solved 734A

---
+++ solutions/734A.cpp
@@ -0,0 +1,25 @@
+// Anton and Danik
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, a=0, d=0;
+    string s;
+    cin >> n >> s;
+
+    for(char c : s){
+        if(c == 'A'){
+            a++;
+        }
+        else d++;
+    }
+
+    if(a>d){
+        cout << "Anton";
+    }
+    else if(d>a){
+        cout << "Danik";
+    }
+    else cout << "Friendship";
+}
